c348a115e24759106be8d6cc28b2d359ff20220d
swagner <swagner@shutterfly.com>
2017-10-03 14:42:32 -0700

    add fake build engine class

---
+++ Build/FakeBuildEngine.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using log4net;
+using Microsoft.Build.Framework;
+
+namespace SBSBuilder.Build
+{
+    public class FakeBuildEngine : IBuildEngine
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof (FakeBuildEngine));
+
+        public bool BuildProjectFile(
+            string projectFileName, string[] targetNames,
+            IDictionary globalProperties,
+            IDictionary targetOutputs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ColumnNumberOfTaskNode
+        {
+            get { return 0; }
+        }
+
+        public bool ContinueOnError
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int LineNumberOfTaskNode
+        {
+            get { return 0; }
+        }
+
+        public void LogCustomEvent(CustomBuildEventArgs e)
+        {
+            Log.Debug(e.Message);
+        }
+
+        public void LogErrorEvent(BuildErrorEventArgs e)
+        {
+            Log.Error(e.Message);
+        }
+
+        public void LogMessageEvent(BuildMessageEventArgs e)
+        {
+            Log.Info(e.Message);
+        }
+
+        public void LogWarningEvent(BuildWarningEventArgs e)
+        {
+            Log.Warn(e.Message);
+        }
+
+        public string ProjectFileOfTaskNode
+        {
+            get { return "fake ProjectFileOfTaskNode"; }
+        }
+    }
+}
